10f4e0c3d644e79240b707babdf6c71659d6c886
Wombatoff <mwombatoff@gmail.com>
2023-04-05 13:59:14 +0300

    Add edit order link in admin panel

--- foodcartapp/admin.py
+++ foodcartapp/admin.py
@@ -1,14 +1,16 @@
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.shortcuts import reverse
 from django.templatetags.static import static
 from django.utils.html import format_html
+from django.utils.http import url_has_allowed_host_and_scheme
 
+from .models import Order
+from .models import OrderProduct
 from .models import Product
 from .models import ProductCategory
 from .models import Restaurant
 from .models import RestaurantMenuItem
-from .models import Order
-from .models import OrderProduct
 
 
 class RestaurantMenuItemInline(admin.TabularInline):
@@ -93,13 +95,16 @@ class ProductAdmin(admin.ModelAdmin):
         if not obj.image:
             return 'выберите картинку'
         return format_html('<img src="{url}" style="max-height: 200px;"/>', url=obj.image.url)
+
     get_image_preview.short_description = 'превью'
 
     def get_image_list_preview(self, obj):
         if not obj.image or not obj.id:
             return 'нет картинки'
         edit_url = reverse('admin:foodcartapp_product_change', args=(obj.id,))
-        return format_html('<a href="{edit_url}"><img src="{src}" style="max-height: 50px;"/></a>', edit_url=edit_url, src=obj.image.url)
+        return format_html('<a href="{edit_url}"><img src="{src}" style="max-height: 50px;"/></a>', edit_url=edit_url,
+                           src=obj.image.url)
+
     get_image_list_preview.short_description = 'превью'
 
 
@@ -131,3 +136,10 @@ class OrderAdmin(admin.ModelAdmin):
         'address',
     )
 
+    def response_change(self, request, obj):
+        response = super().response_change(request, obj)
+        next_url = request.GET.get('next')
+        if next_url and url_has_allowed_host_and_scheme(next_url, allowed_hosts=None,
+                                                        require_https=request.is_secure()):
+            return redirect(next_url)
+        return response
