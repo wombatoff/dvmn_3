850a7affca1e6a5767ef3c4c7ae7aefefdd2e77a
Wombatoff <mwombatoff@gmail.com>
2023-06-08 11:46:24 +0300

    Rollbar settings update: Defaults and optional configuration

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -14,7 +14,6 @@ SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG', False)
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1', 'localhost'])
 DATABASE_URL = env('DATABASE_URL')
-ROLLBAR_ACCESS_TOKEN = env('ROLLBAR_ACCESS_TOKEN')
 
 INSTALLED_APPS = [
     'foodcartapp.apps.FoodcartappConfig',
@@ -43,7 +42,7 @@ MIDDLEWARE = [
 ]
 
 ROLLBAR = {
-    'access_token': ROLLBAR_ACCESS_TOKEN,
+    'access_token': None,
     'code_version': '1.0',
     'root': BASE_DIR,
 }
